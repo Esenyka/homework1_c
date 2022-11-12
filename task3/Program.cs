94057dfddac168d8ec8d26effee01c3100d5e76d
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-11-12 15:06:40 +0700

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Да, четное");
+}
+else
+{
+    Console.WriteLine("Нет, не четное");
+}
+
