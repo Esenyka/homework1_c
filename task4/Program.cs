4711bfd4fe0ece445f6ffa28aa8e53f782788bf2
eJwtzE2PwiAUheH/wroLaCVt2fqRzGRiYsbJLAnQq0OLXIRWR43/XWK6OznJ8z6IMgZS2uMAngjSVhraqqYtVbXm2tS0YcArxjkwMIsDb0ooDWWkICHixXYQZ/qYS3KcU8c/lBu/6rE/19vzT7P6aod135f7z+Vmd/+4XwI1v9/M59SbyPEWIDsNKkLMbzL4PlR3sl6ESTtr5AC3IoLqBMZjHgGLKUEUcFLWFVeMw8HhNWP4DzZCkmokwk/OPZ8vJpVP6w== <esenia1317@gmail.com>
2022-11-12 15:20:57 +0700

    added task4

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+
+while (count != number)
+{
+    if (count % 2 == 0 && count != 0)
+    {
+        Console.WriteLine(count);
+        count++;
+    }
+    else
+        count++;
+}
